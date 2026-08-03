06f9aded0853d85966b074e3e14a0293cfb7d4a0
alexssander-cristino <alexssander.cristino@unidavi.edu.br>
2026-08-03 20:54:37 -0300

    Atualiza versão APK

--- backend/app/Http/Controllers/Api/CategoriaEventoController.php
+++ backend/app/Http/Controllers/Api/CategoriaEventoController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\CategoriaEvento;
+use Illuminate\Http\Request;
 
 
 class CategoriaEventoController extends Controller
@@ -14,10 +15,81 @@ class CategoriaEventoController extends Controller
     {
 
         return response()->json(
-
-            CategoriaEvento::orderBy('nome')->get()
-
+            CategoriaEvento::all()
         );
+
+    }
+
+
+
+
+
+    public function store(Request $request)
+    {
+
+        $request->validate([
+
+            'nome'=>'required'
+
+        ]);
+
+
+
+        $categoria = CategoriaEvento::create([
+
+            'nome'=>$request->nome
+
+        ]);
+
+
+
+        return response()->json(
+            $categoria,
+            201
+        );
+
+    }
+
+
+
+
+
+    public function update(Request $request, $id)
+    {
+
+        $categoria =
+            CategoriaEvento::findOrFail($id);
+
+
+        $categoria->update([
+
+            'nome'=>$request->nome
+
+        ]);
+
+
+        return response()->json(
+            $categoria
+        );
+
+    }
+
+
+
+
+
+    public function destroy($id)
+    {
+
+        CategoriaEvento::findOrFail($id)
+            ->delete();
+
+
+        return response()->json([
+
+            'message'=>'Categoria removida'
+
+        ]);
 
     }
 
